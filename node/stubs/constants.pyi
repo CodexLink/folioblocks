9d4c665a5cbf398133337bf8c0f78b1f9220743f
Janrey Licas <self.codexlink@gmail.com>
2022-02-07 19:35:23 +0800

    Typing (stubs): Updated.

--- node/stubs/constants.pyi
+++ node/stubs/constants.pyi
@@ -1,39 +1,82 @@
-from enum import IntEnum
+from enum import Enum, IntEnum
 from typing import Any, Final
 
+class DocToRequestTypes(IntEnum):
+    TOR: int
+    SPECIFIED: int
+NotificationContext = list[dict[str, Any]]
+RoleContext = dict[str, Any]
+DocumentSet = list[dict[str, Any]]
+AcademicExperience: Any
 AddressUUID: Any
 ArgumentParameter: Any
 ArgumentDescription: Any
 BlockID: Any
+Certificates: Any
+CredentialContext: Any
+DocRequestType: Any
+Documents: Any
+DocumentMeta: Any
+DocumentProof: Any
+GenericUUID: Any
+HashUUID: Any
+InternExperience: Any
 NodeRoles: Any
-URLAddress: Any
+JWTToken: Any
 ProgramMetadata: Any
+RequestContext: Any
+URLAddress: Any
+UserRole: Any
 TxID: Any
+WorkExperience: Any
 NODE_LIMIT_NETWORK: Final[int]
 NODE_IP_URL_TARGET: Final[str]
 NODE_IP_PORT_FLOOR: int
 NODE_ROLE_CHOICES: Final[list[NodeRoles]]
 
-class DashboardAPITags(IntEnum):
-    GENERAL_API: int
-    CLIENT_ONLY_API: int
-    APPLICANT_ONLY_API: int
-    EMPLOYER_ONLY_API: int
-    INSTITUTION_ONLY_API: int
+class BaseAPI(Enum):
+    DASHBOARD: str
+    EXPLORER: str
+    NODE: str
 
-class ExplorerAPITags(IntEnum):
-    GENERAL_FETCH: int
-    LIST_FETCH: int
-    SPECIFIC_FETCH: int
-    BLOCK_FETCH: int
-    TRANSACTION_FETCH: int
-    ADDRESS_FETCH: int
+class DashboardAPI(Enum):
+    DASHBOARD_GENERAL_API: str
+    CLIENT_ONLY_API: str
+    APPLICANT_ONLY_API: str
+    EMPLOYER_ONLY_API: str
+    INSTITUTION_ONLY_API: str
 
-class NodeAPITags(IntEnum):
-    GENERAL_NODE_API: int
-    MASTER_NODE_API: int
-    SIDE_NODE_API: int
-    NODE_TO_NODE_API: int
+class ExplorerAPI(Enum):
+    GENERAL_FETCH: str
+    LIST_FETCH: str
+    SPECIFIC_FETCH: str
+    BLOCK_FETCH: str
+    TRANSACTION_FETCH: str
+    ADDRESS_FETCH: str
+
+class NodeAPI(Enum):
+    GENERAL_NODE_API: str
+    MASTER_NODE_API: str
+    SIDE_NODE_API: str
+    NODE_TO_NODE_API: str
+
+class TransactionStatus(IntEnum):
+    PENDING: int
+    SUCCESS: int
+    FAILED: int
+
+class TransactionActions(IntEnum):
+    ACCOUNT_GENERATED: int
+    DATA_UPDATED: int
+    DATA_DISREGARDED: int
+    DATA_BATCH_MINTING: int
+    DOCUMENT_INSUANCE: int
+    REQUEST_INITIATION: int
+    REQUEST_PROCESSING: int
+    REQUEST_MARKED_ENDED: int
+    REQUEST_SPECIFIC_DOC: int
+
+class TransactionActionString(Enum): ...
 
 class ItemReturnCount(IntEnum):
     LOW: Final[int]
--- node/stubs/main.pyi
+++ node/stubs/main.pyi
@@ -1,7 +1,5 @@
-from apis.dashboard import dashboard_router as dashboard_router
-from apis.explorer import explorer_router as explorer_router
-from apis.node import node_router as node_router
-from node.utils.constants import NODE_IP_PORT_FLOOR as NODE_IP_PORT_FLOOR
-from typing import Any
+from argparse import Namespace
+from fastapi import FastAPI
 
-api: Any
+parsed_args: Namespace
+api: FastAPI
